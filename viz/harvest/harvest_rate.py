b246c7397b0cc661cac1968421588db2bc6747b5
Christine Doig <christine.doig.cardet@gmail.com>
2014-09-22 09:26:01 -0400

    add harvest rate plot and move initial termite scripts

--- viz/harvest/harvest_rate.py
+++ viz/harvest/harvest_rate.py
@@ -3,8 +3,11 @@ import sys
 from blaze import *
 import pandas as pd
 from bokeh.plotting import *
+from bokeh.objects import HoverTool
+from collections import OrderedDict
 import numpy as np
 import datetime as dt
+from __future__ import division
 
 def generate_harvest():
     """
@@ -23,11 +26,11 @@ class Harvest(object):
         t = Table(CSV(self.harvest_data, columns=['relevant_pages', 'downloaded_pages', 'timestamp']))
         t = transform(t, timestamp=t.timestamp.map(dt.datetime.fromtimestamp, schema='{timestamp: datetime}'))
         t = transform(t, date=t.timestamp.map(lambda x: x.date(), schema='{date: date}'))
-        t = transform(t, harvest_rate=t.relevant_pages/r.downloaded_pages))
+        #t = transform(t, harvest_rate=t.relevant_pages/t.downloaded_pages)
 
         relevant_pages = into(np.ndarray, t.relevant_pages)
         downloaded_pages = into(np.ndarray, t.downloaded_pages)
-        harvest_rate = into(np.array, t.harvest_rate)
+        harvest_rate = relevant_pages/downloaded_pages
         timestamp = into(np.ndarray, t.timestamp)
 
         source = ColumnDataSource(
@@ -43,21 +46,31 @@ class Harvest(object):
 
         #data_source = into(ColumnDataSource, data)
 
-        figure(plot_width=800, plot_height=500, title="Harvest Plot", tools='resize, save', x_axis_type='datetime')
+        figure(plot_width=800, plot_height=500, title="Harvest Plot", tools='pan, wheel_zoom, box_zoom, reset, resize, save, hover', x_axis_type='datetime')
         hold()
 
-        scatter(x="timestamp", y="relevant_pages", fill_alpha=0.6, color="red", width=0.2, source=source)
-        line(x="timestamp", y="relevant_pages", fill_alpha=0.6, color="red", width=0.2, legend="relevant", source=source)
-        scatter(x="timestamp", y="downloaded_pages", fill_alpha=0.6, color="blue", width=0.2, source=source)
-        line(x="timestamp", y="downloaded_pages", fill_alpha=0.6, color="blue", width=0.2, legend="downloaded", source=source)
+        scatter(x="timestamp", y="relevant_pages", fill_alpha=0.6, color="red", source=source)
+        line(x="timestamp", y="relevant_pages", color="red", width=0.2, legend="relevant", source=source)
+        scatter(x="timestamp", y="downloaded_pages", fill_alpha=0.6, color="blue", source=source)
+        line(x="timestamp", y="downloaded_pages", color="blue", width=0.2, legend="downloaded", source=source)
 
         hover = curplot().select(dict(type=HoverTool))
         hover.tooltips = OrderedDict([
-            ("(x,y)", "($x, $y)"),
-            ("fill color", "$color[hex, swatch]:colors"),
+            ("harvest_rate", "@harvest_rate"),
         ])
 
         legend().orientation = "top_left"
+        show()
+
+        figure(plot_width=800, plot_height=500, title="Harvest Rate", x_axis_type='datetime', tools='pan, wheel_zoom, box_zoom, reset, resize, save, hover')
+        line(x="timestamp", y="harvest_rate", fill_alpha=0.6, color="blue", width=0.2, legend="harvest_rate", source=source)
+        scatter(x="timestamp", y="harvest_rate", alpha=0, color="blue", legend="harvest_rate", source=source)
+
+        hover = curplot().select(dict(type=HoverTool))
+        hover.tooltips = OrderedDict([
+            ("harvest_rate", "@harvest_rate"),
+        ])
+
         show()
 
         return curplot()
