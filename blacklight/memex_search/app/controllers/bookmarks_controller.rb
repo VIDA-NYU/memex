80bcdc99ddc00ad6acfc7ec21bddc5e8f9839d1e
Kien Pham <kienpham@gray17.poly.edu>
2014-12-28 20:04:21 -0500

    add search interface (based on blacklight opensource)

---
+++ blacklight/memex_search/app/controllers/bookmarks_controller.rb
@@ -0,0 +1,6 @@
+#-*- encoding : utf-8 -*-
+class BookmarksController < CatalogController  
+
+  include Blacklight::Bookmarks
+
+  end 
---
+++ blacklight/memex_search/config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = 'f8eda90bb2c5f2b0687ddba81223bad0911b9512cca6b3d27353e60b6b9879c4dffaba868849ca938b4e7ad3806e98cfabe2f63b728b2f84010dc2edc87d7a15'
+
