d886dd45aa774352cbdc83e150cd374b5377333a
Kien Pham <kienpham@gray17.poly.edu>
2015-01-31 16:13:21 -0500

    remove crawler from the repository

--- focused_crawler/script/build_model.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIRECTORY=$1 #Directory that contain training examples. it should have two subdirectories: positive and negative
-OUTPUT=$2 #output directory
-mkdir -p $OUTPUT
-java -cp "class/:libs/weka-stable-3.6.10.jar" focusedCrawler.target.CreateWekaInput conf/conf_default/target_storage/target_storage.cfg ${DIRECTORY} ${DIRECTORY}/weka.arff
-java -cp libs/weka-stable-3.6.10.jar weka.classifiers.functions.SMO -M -d ${OUTPUT}/pageclassifier.model -t ${DIRECTORY}/weka.arff
-echo "CLASS_VALUES  S NS" > ${OUTPUT}/pageclassifier.features
-echo -n "ATTRIBUTES " >> ${OUTPUT}/pageclassifier.features
-cat ${DIRECTORY}/weka.arff | awk '{FS=" "; if (($1=="@ATTRIBUTE") && ($3=="REAL")) {print $2}}' | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' >> ${OUTPUT}/pageclassifier.features
--- focused_crawler/script/stop_crawler.sh
+++
@@ -1,9 +0,0 @@
-if [ $# -eq 1 ]
-then
-    CRAWLER_NAME=$1
-else
-    #default parameters
-    CRAWLER_NAME='default-achecrawler'
-fi
-echo "Shut down the crawler "$1" ...."
-kill $(ps aux | grep $CRAWLER_NAME | grep -v "grep" | awk '{print $2}')
