8c8285a9f23d5384b54f4349a11b571dbb2c20f3
Kien Pham <kienpham@gray17.poly.edu>
2015-02-10 06:20:20 -0500

    change name of the compile script

--- analytics/lda_pipeline/run.sh
+++ analytics/lda_pipeline/run.sh
@@ -1,4 +1,4 @@
-sh compile_Extract.sh
+sh compile.sh
 mkdir -p data
 LDADATA="data/lda_input.csv"
 #java -cp .:class/:lib/boilerpipe-1.2.0.jar:lib/nekohtml-1.9.13.jar:lib/xerces-2.9.1.jar Extract ../seeds_generator/html/ 
