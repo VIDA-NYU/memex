8a8bd0d62277b0767c2f35fa4e3180748ddd7df6
Kien Pham <kienpt.vie@gmail.com>
2015-02-16 16:49:18 -0500

    update dependencies

--- pageclassifier/script/compile.sh
+++ pageclassifier/script/compile.sh
@@ -1,4 +1,4 @@
 mkdir -p class
 find ./src/ -name "*.java" > sources.txt
-javac -d class -encoding ISO-8859-1 -cp .:libs/weka.jar:libs/nekohtml.jar:libs/xercesImpl.jar @sources.txt
+javac -d class -encoding ISO-8859-1 -cp .:libs/weka-stable-3.6.10.jar:libs/nekohtml-1.9.20.jar:libs/xercesImpl-2.11.0.jar @sources.txt
 rm -rf sources.txt
