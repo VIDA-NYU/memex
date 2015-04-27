b24d56bf5c38de01670f1b930c78c033f4040a27
Remi Rampin <remirampin@gmail.com>
2015-04-27 15:07:45 -0400

    Change venv creation

--- seed_crawler/fabfile.py
+++ seed_crawler/fabfile.py
@@ -1,26 +1,20 @@
 from fabric.api import env, local, lcd, shell_env
 from fabric.colors import red, green
 from fabric.decorators import task, runs_once
-from fabric.operations import prompt
-from fabric.utils import abort
-from zipfile import ZipFile
+from fabric.utils import abort, puts
 
-import datetime
 import fileinput
-import importlib
 import os
-import random
-import re
-import subprocess
 import sys
-import time
 
 PROJ_ROOT = os.path.dirname(env.real_fabfile)
 MEMEX_ROOT = os.path.join(PROJ_ROOT, '..')
 env.project_name = 'seed_crawler'
-env.python = 'python' if 'VIRTUAL_ENV' in os.environ else 'bin/python'
-env.nltk_data = PROJ_ROOT + '/nltk_data'
-env.pythonpath = PROJ_ROOT + '/seeds_generator/src:.'
+env.python = 'python' if 'VIRTUAL_ENV' in os.environ else 'venv/bin/python'
+env.nltk_data = os.path.join(PROJ_ROOT, 'nltk_data')
+env.pythonpath = os.pathsep.join([
+    os.path.join(PROJ_ROOT, 'seeds_generator', 'src'),
+    '.'])
 
 
 @task
@@ -37,9 +31,8 @@ def setup():
     make_virtual_env()
     install_nltk_data()
     compile_seeds_generator()
-    symlink_packages()
     #install_node_packages()
-    print '\nDevelopment environment successfully created.'
+    puts(green('Development environment successfully created.'))
 
 @task
 @runs_once
@@ -54,9 +47,9 @@ def make_settings():
         local('if [ ! -f {0} ]; then cp {1} {0}; fi'.format(
             settings_file, 'vis/config.conf-in'))
         for line in fileinput.input(settings_file, inplace=True):
-            print line.replace("tools.staticdir.root = .",
-                               "tools.staticdir.root = {0}/{1}".format(
-                                   PROJ_ROOT, 'vis/html')),
+            puts(line.replace("tools.staticdir.root = .",
+                              "tools.staticdir.root = {0}/{1}".format(
+                                  PROJ_ROOT, 'vis/html')))
 
 @task
 def runserver():
@@ -78,9 +71,9 @@ def runvis():
 def make_virtual_env():
     "Make a virtual environment for local dev use"
     with lcd(PROJ_ROOT):
-        local('virtualenv --system-site-packages .')
-        local('./bin/pip install -r requirements.pre.txt')
-        local('./bin/pip install -r requirements.txt')
+        local('virtualenv --system-site-packages venv')
+        local('venv/bin/pip install -r requirements.pre.txt')
+        local('venv/bin/pip install -r requirements.txt')
 
 def install_node_packages():
     "Install requirements from NPM."
@@ -98,20 +91,3 @@ def compile_seeds_generator():
     "Compile the sees generator."
     with lcd(PROJ_ROOT + '/seeds_generator'):
         local('sh compile.sh')
-
-def symlink_packages():
-    "Symlink python packages not installed with pip"
-    missing = []
-    requirements = (req.rstrip().replace('# symlink: ', '')
-                    for req in open('requirements.txt', 'r')
-                    if req.startswith('# symlink: '))
-    for req in requirements:
-        try:
-            module = importlib.import_module(req)
-        except ImportError:
-            missing.append(req)
-            continue
-        with lcd(os.path.join(PROJ_ROOT, 'lib', 'python2.7', 'site-packages')):
-            local('ln -f -s {}'.format(os.path.dirname(module.__file__)))
-    if missing:
-        abort('Missing python packages: {}'.format(', '.join(missing)))
